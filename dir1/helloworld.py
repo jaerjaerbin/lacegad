eb449372fcd5e744c7f0854fb81b81517057f665
alucardthefish <sergiop18@gmail.com>
2018-09-22 01:52:15 -0500

    agregando funcion que da un saludo a una persona particular

--- dir1/helloworld.py
+++ dir1/helloworld.py
@@ -3,3 +3,6 @@
 def holamundo(nombre):
     mensaje = "Hola Mundo! gracias por tanta belleza."
     print(mensaje + " att." + str(nombre))
+
+def saludoPara(nombre):
+    print("esto es un saludo para: " + str(nombre))
