e75e755d4f367025fc590fe341e935615cf5eaca
alucardthefish <sergiop18@gmail.com>
2019-01-02 22:03:59 -0500

    Agregando funcion chaomundo y cambiando el nombre del archivo.

--- dir1/utils.py
+++ dir1/utils.py
@@ -9,3 +9,6 @@ def saludoPara(nombre):
 
 def despedidaPara(nombre):
     print("Nos vemos en otra oportunidad " + str(nombre) + ", te cuidas")
+
+def chaomundo(nombre):
+    print("Adios mundo cruel! Att. " + str(nombre))
